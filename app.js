3b65d3876bcaeb3e11874c5d4c7e84a8e5a8a715
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-22 23:03:25 +0530

    Using fs Read File and Send Data to APiRoute

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) =>
-  res.status(200).json({
-    message: 'Request Response from Server Side',
-    App: 'Natours',
-  })
-);
+// Simple GET POST Request
+// app.get('/', (req, res) =>
+//   res.status(200).json({
+//     message: 'Request Response from Server Side',
+//     App: 'Natours',
+//   })
+// );
 
-app.post('/', (req, res) => res.send('Post Request Response'));
+// app.post('/', (req, res) => res.send('Post Request Response'));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+
 const port = 3000;
 app.listen(3000, () => console.log(`Hello from server ${port}`));
