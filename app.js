0db0bfec3c00c165a56ad42e06247ec51d537fb4
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-22 22:24:37 +0530

    Json Object Response of Get Req

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).send('Request Response'));
+app.get('/', (req, res) =>
+  res.status(200).json({
+    message: 'Request Response from Server Side',
+    App: 'Natours',
+  })
+);
 const port = 3000;
 app.listen(3000, () => console.log(`Hello from server ${port}`));
