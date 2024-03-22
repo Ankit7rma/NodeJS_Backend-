4458491874dab787523a9fdf0d3b93a9606a6242
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-22 22:21:04 +0530

    Get Req

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.status(200).send('Request Response'));
 const port = 3000;
 app.listen(3000, () => console.log(`Hello from server ${port}`));
