e773eefe5f3c41d65a8680f277c1e60fe5a6f5d0
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-22 22:27:35 +0530

    Post Response

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ app.get('/', (req, res) =>
     App: 'Natours',
   })
 );
+
+app.post('/', (req, res) => res.send('Post Request Response'));
 const port = 3000;
 app.listen(3000, () => console.log(`Hello from server ${port}`));
