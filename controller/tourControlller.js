883a8f148564783c79c3d238a2f06b5f359eb523
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-23 13:03:52 +0530

    CheckBody middleware

--- controller/tourControlller.js
+++ controller/tourControlller.js
@@ -13,6 +13,15 @@ exports.checkId = (req, res, next, val) => {
     });
   }
 };
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing Name or Price',
+    });
+  }
+  next();
+};
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const tourController = require('../controller/tourControlller');
-const { getAllTours, createTour, getTour, updateTour, deleteTour, checkId } =
-  tourController;
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  checkId,
+  checkBody,
+} = tourController;
 
 router.param('id', checkId);
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(getAllTours).post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
