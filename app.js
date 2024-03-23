388b495c10e900e1754cd2f4ce6705f63c5af0a0
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-23 13:09:56 +0530

    serve Static files

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userRoute = require('./routes/userRoute');
 // MiddleWare
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('Hello from Middleware');
   next();
