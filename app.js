0a401cccebfe4c2cc4fc783287944c4f85ee46b1
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-23 08:07:20 +0530

    Post Req with created a newTour in json file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 // MiddleWare
 app.use(express.json());
+
 // Simple GET POST Request
 // app.get('/', (req, res) =>
 //   res.status(200).json({
@@ -13,9 +14,11 @@ app.use(express.json());
 // );
 
 // app.post('/', (req, res) => res.send('Post Request Response'));
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'Success',
@@ -25,8 +28,25 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Post request finished');
+  //   console.log(req.body);
+  const tourId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: tourId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  //   res.send('Post request finished');
 });
 
 const port = 3000;
