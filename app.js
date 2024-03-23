d5027c8a06a4ece5d7dc44c96169baf7f8712555
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-23 08:56:42 +0530

    delete req

--- app.js
+++ app.js
@@ -83,6 +83,18 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     },
   });
 });
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'Success',
+    data: null,
+  });
+});
 
 const port = 3000;
 app.listen(3000, () => console.log(`Hello from server ${port}`));
