cfcb5777624048309fe061725f7158b20d714675
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-25 12:53:45 +0530

    MVC architecture

--- server.js
+++ server.js
@@ -16,37 +16,21 @@ mongoose
   })
   .then((con) => {
     // console.log(con.connection);
-    console.log('Connection Done Succesfully');
+    console.log('DB Connection Done Succesfully');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name '],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
+// For Testing
 
-const Tour = mongoose.model('Tour', tourSchema);
+// const testTour = new Tour({
+//   name: 'The Forest Hiker',
+//   rating: 4.3,
+//   price: 400,
+// });
 
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.3,
-  price: 400,
-});
-
-testTour
-  .save()
-  .then((doc) => console.log(doc))
-  .catch((err) => console.log('Error🔥', err));
+// testTour
+//   .save()
+//   .then((doc) => console.log(doc))
+//   .catch((err) => console.log('Error🔥', err));
 
 const port = process.env.Node || 3000;
 app.listen(3000, () => console.log(`Hello from server ${port}`));
