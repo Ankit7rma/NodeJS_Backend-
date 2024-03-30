8e555fe283ad5fd65aa3d4810029bda96849992d
Ankit Sharma <ankitsharma290101@gmail.com>
2024-03-30 20:41:16 +0530

    Global; Error Handling MIddleware

--- app.js
+++ app.js
@@ -27,10 +27,26 @@ app.use('/api/v1/users', userRoute);
 
 // Handling Unhandled Routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.statusCode = 404;
+  err.status = 'fail';
+  next(err);
+});
+
+// Error Handling
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
+  next();
 });
 
 //   Start Server
